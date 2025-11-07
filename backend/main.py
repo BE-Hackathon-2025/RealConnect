703b3c78eed9ea1ad6427a295f050cbecf5c9541
GreenHexagon <irrelevantpersonindividual@gmail.com>
2025-11-07 13:22:59 -0500

    Main.py edit.

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,32 @@
 import fastapi
 
 from typing import Union
+from fastapi.middleware.cors import CORSMiddleware
 
 backend = fastapi.FastAPI()
+origins = [
+    "https://localhost:3000", 
+]
+
+backend.add_middleware(
+    CORSMiddleware, 
+    allow_origins= origins,
+    allow_credentials = True,
+    allow_methods = ["POST", "GET",], 
+    allow_headers = ["*"]
+)
 
 @backend.get("/")
 def root():
     return "Hello World"
 
+@backend.post("/api/ai/search")
+def aiServices():
+    return {}
+
 @backend.get("api/services")
 def services():
     return {"I am a teapot.": "I am a teapot."}
-
-@backend.post("api/ai/recommendations")
 
 @backend.post("api/services/search")
 def searchServices():
@@ -20,8 +34,9 @@ def searchServices():
 
 @backend.post("/api/community/message")
 def sendMessage():
-    pass
+    return {}
 
 @backend.get("/api/community/wall")
 def get_message():
     return {"Wall": {"Brick": "Cement"}}
+
